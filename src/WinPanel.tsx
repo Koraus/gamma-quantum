5aecc6cb01a5f1ea90d8db8ad21e49e4dea87475
Koraus <dmytro.kashchynets@gmail.com>
2023-03-07 10:59:54 +0100

    Added problemProgress

--- src/WinPanel.tsx
+++ src/WinPanel.tsx
@@ -13,15 +13,29 @@ export function WinPanel() {
     const [win, setWin] = useRecoilState(winRecoil);
 
     const world = useWorld();
-    useEffect(() => { if (!win && isWin(world)) { setWin(true); }});
+    useEffect(() => { if (!win && isWin(world)) { setWin(true); } });
 
-    if (!win) { return null; }
-    return <div className={cx(
-        css({
-            fontWeight: "bold",
-            fontSize: "16px",
-            color: "green",
-            width: "fit-content",
-        }),
-    )}>  'Win' </div>;
+    const problemProgress = () => {
+        return Object.entries(world.problem.demand).map(
+            ([key, val], i) => {
+                if (Object.entries(world.consumed).length > 0) {
+                    return Object.entries(world.consumed)
+                        .map(([kC, vC], iC) => {
+                            if (key === kC) {
+                                return <div key={i}>{key} : {vC} / {val} </div>;
+                            }
+                        });
+                } else return <div key={i}>{key} : 0 / {val} </div>;
+            },
+        );
+    };
+    return <> problemProgress :  {problemProgress()}
+        {win && <div className={cx(
+            css({
+                fontWeight: "bold",
+                fontSize: "16px",
+                color: "green",
+                width: "fit-content",
+            }),
+        )}>  'Win' </div>} </>;
 }
