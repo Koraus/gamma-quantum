ef110c1a7fe0dfbed7d9af3b80138e9c776868d3
Koraus <dmytro.kashchynets@gmail.com>
2023-03-15 11:02:29 +0100

    useWindowKeyDown fixed

--- src/utils/useWindowKeyDown.tsx
+++ src/utils/useWindowKeyDown.tsx
@@ -1,12 +1,12 @@
 import { useEffect } from "react";
 
-export default function useWindowKeyDown(keyCode: string, handler) {
+export default 
+function useWindowKeyDown(keyCode: string, handler:  () => void) {
     useEffect(() => {
         const onKeyDown = (e: KeyboardEvent) => {
             if (e.code === keyCode) handler();
         };
         window.addEventListener("keydown", onKeyDown);
         return () => window.removeEventListener("keydown", onKeyDown);
-    });
-
+    },[]);
 }
