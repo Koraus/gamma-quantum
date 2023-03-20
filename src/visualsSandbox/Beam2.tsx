e20ba541ca06b7f3f050797bd67b1c397d654e25
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2023-03-21 00:35:59 +0200

    Beam2 played with noise

--- src/visualsSandbox/Beam2.tsx
+++ src/visualsSandbox/Beam2.tsx
@@ -1,13 +1,15 @@
 import { CameraControls, GizmoHelper, GizmoViewport, Grid, PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useControls } from "leva";
-import { AddEquation, AdditiveBlending, BackSide, Color, CustomBlending, CylinderGeometry, DstAlphaFactor, Group, InstancedMesh, Material, Mesh, MeshBasicMaterial, OneMinusSrcAlphaFactor, PlaneGeometry, ShaderChunk, ShaderMaterial, SphereGeometry, SrcAlphaFactor, WebGLRenderTarget, WebGLRenderer } from "three";
+import { AddEquation, AdditiveBlending, BackSide, BufferGeometryUtils, Color, CustomBlending, CylinderGeometry, DstAlphaFactor, Group, InstancedMesh, Material, Mesh, MeshBasicMaterial, OneMinusSrcAlphaFactor, PlaneGeometry, ShaderChunk, ShaderMaterial, SphereGeometry, SrcAlphaFactor, WebGLRenderTarget, WebGLRenderer } from "three";
 // import { FullScreenQuad } from "three/examples/jsm/postprocessing/Pass";
 import { randomChunk } from "./shaderChunks/randomChunk";
 import { easingChunk } from "./shaderChunks/easingChunk";
 import { zoneChunk } from "./shaderChunks/zoneChunk";
 import { transformChunk } from "./shaderChunks/transformChunk";
 import { useEffect, useRef } from "react";
+import { noise2DChunk } from "./shaderChunks/noise2DChunk";
+import { noise3DChunk } from "./shaderChunks/noise3DChunk";
 
 // based on https://github.com/mrdoob/three.js/blob/b9bc47ab1978022ab0947a9bce1b1209769b8d91/src/renderers/webgl/WebGLProgram.js#L204
 export const resolveShaderIncludes = (string: string): string =>
@@ -40,7 +42,7 @@ export function BeamImpl({
         g.add(g1);
 
         const material = new ShaderMaterial({
-            side: BackSide,
+            // side: BackSide,
             depthWrite: false,
             transparent: true,
             uniforms: {
@@ -48,17 +50,29 @@ export function BeamImpl({
             },
             vertexShader: /*glsl*/`
 #include <common>
+${noise3DChunk}
 uniform float timeSec;
 varying vec2 vUv;
 varying vec3 transformedNormal;
+varying float radiusFactor;
+varying vec3 vPosition;
 
 void main() {
-    float radiusFactor = (1.0 + 0.2 * cos(position.y / 50.0 / PI * 2.0));
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( 
+    vec3 tPosition = position;
+    tPosition += timeSec;
+    radiusFactor = 1.0
+        + 2.0 * abs(cos((position.y / 200.0)))
+        + 0.3 * (snoise(tPosition * 0.1)
+        + snoise(tPosition * 0.02) * 2.0);
+    vec3 tPosition1 = vec3( 
         position.x * radiusFactor,
         position.y,
-        position.z * radiusFactor,
+        position.z * radiusFactor);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( 
+        tPosition1,
         1.0);
+
+    vPosition = tPosition1;
 
     vUv = uv.xy;
     transformedNormal = normalize(vec3(position.x, 0.0, position.z));
@@ -67,17 +81,37 @@ void main() {
 }
 `,
             fragmentShader: /*glsl*/`
+#include <common>
+${noise3DChunk}
+uniform float timeSec;
 varying vec2 vUv;
 varying vec3 transformedNormal;
+varying float radiusFactor;
+varying vec3 vPosition;
 
 void main() {
-    gl_FragColor = vec4( 
-        transformedNormal, 
-        transformedNormal.z > 0.0 ? pow(transformedNormal.z, 5.0) : 0.0);
+    if (transformedNormal.z < 0.0) {
+        gl_FragColor = vec4(1.0, 1.0, 1.0, abs(transformedNormal.z));
+        return;
+    }
+    float a = pow(abs(transformedNormal.z - 0.1) * 0.8, 1.0);
+    vec3 color = vec3(
+        snoise(vPosition * 0.001)
+        + snoise(vPosition * 0.02) * 2.0 * transformedNormal.x,
+    a + snoise(vPosition * 0.1)
+        + snoise(vPosition * 0.02) * 2.0 * transformedNormal.y,
+    snoise(vPosition * 10.0)
+        + snoise(vPosition * 1.2) * 2.0 * transformedNormal.z);
+    color = (1.0 - color) * (1.0 - color);
+    gl_FragColor = vec4(
+        1.0, 1.0, 1.0,
+        a *         
+            (snoise(vPosition * 13.0)
+            + snoise(vPosition * 0.21) * 2.0));
 }
 `,
         });
-        const geometry = new CylinderGeometry(20, 20, 2500, 32, 500, true);
+        const geometry = new CylinderGeometry(20, 20, 2500, 128, 1000, true);
 
         const obj = new Mesh(geometry, material);
         const objG = new Group();
@@ -95,7 +129,7 @@ void main() {
 
         const update = () => {
             if (run) {
-                // material.uniforms.timeSec.value = performance.now() * 0.001;
+                material.uniforms.timeSec.value = performance.now() * 0.001;
             }
             h = requestAnimationFrame(update);
         }
