76a7e0328716bec5d0e4d691fc36803f3d9d1e52
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2023-03-25 16:10:27 +0200

    smoother movement

--- src/app/scene/MainScene.tsx
+++ src/app/scene/MainScene.tsx
@@ -19,6 +19,9 @@ import { useFrame } from "@react-three/fiber";
 import { cellContentRecoil } from "./cellContentRecoil";
 import { heldKeys } from "../../utils/heldKeys";
 
+
+const lerp = (a: number, b: number, t: number) =>
+    a + t * (b - a);
 const x0y = ([x, y]: v2 | v3) => tuple(x, 0, y);
 
 export const axialToFlatCartXz =
@@ -100,24 +103,48 @@ export function MainScene() {
                     onFrame={g => {
                         const t = nowPlaytime(playAction) - world.step;
 
-                        if (!prev) {
-                            // appear
-                            g.scale.setScalar(easeBackOut(t));
-                            g.position.set(...x0y(toFlatCart(p.position)));
-                        } else if (p.isRemoved) {
-                            // disappear
-                            g.scale.setScalar(1 - easeBackIn(t));
-                            g.position.set(...x0y(toFlatCart(p.position)));
-                        } else {
-                            // move
-                            g.scale.setScalar(1);
+                        if (world.action === "move" && prev) {
                             g.position.set(
                                 ...x0y(toFlatCart(prev.position)));
                             g.position.lerp(
-                                new Vector3(
-                                    ...x0y(toFlatCart(p.position))),
-                                easeSinInOut(t));
-                            g.position.y = 0.1 + j * 0.2;
+                                new Vector3(...x0y(toFlatCart(p.position))),
+                                lerp(0.25, 0.75, t),
+                            );
+                        } else {
+                            if (t < 0.5) {
+                                if (prev) {
+                                    g.position.set(
+                                        ...x0y(toFlatCart(
+                                            v2.sub(
+                                                prev.position,
+                                                prev.velocity))));
+                                    g.position.lerp(
+                                        new Vector3(...x0y(toFlatCart(
+                                            prev.position))),
+                                        lerp(0.75, 1.25, t),
+                                    );
+                                }
+                            } else {
+                                g.position.set(
+                                    ...x0y(toFlatCart(p.position)));
+                                g.position.lerp(
+                                    new Vector3(...x0y(toFlatCart(
+                                        v2.add(p.position, p.velocity)))),
+                                    lerp(-0.25, 0.25, t),
+                                );
+                            }
+                        }
+                        g.position.y = 0.1 + j * 0.2;
+
+                        if (!prev) {
+                            // appear
+                            g.scale.setScalar(easeBackOut(t));
+                        } else if (p.isRemoved) {
+                            // disappear
+                            g.scale.setScalar(1 - easeBackIn(t));
+                        } else {
+                            // move
+                            g.scale.setScalar(1);
                         }
 
                     }}
