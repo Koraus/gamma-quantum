d38be065bdbc3ed09b52aebfedb2c88c9425d6ff
Koraus <dmytro.kashchynets@gmail.com>
2023-04-06 12:45:07 +0200

    Update orange glow to the trap

--- src/app/worldOnScene/WorldOnScene.tsx
+++ src/app/worldOnScene/WorldOnScene.tsx
@@ -176,38 +176,48 @@ export function WorldOnScene() {
                         position={[0, 0.05, 0]}
                         renderOrder={-1}
                     ></mesh>
-                    <mesh rotation={[0, Math.PI / 4, 0]}>
-                        <boxGeometry args={[0.5, 10, 0.1]} />
-                        <meshBasicMaterial
-                            side={DoubleSide}
-                            color={"#FF6F1E"}
-                            transparent
-                            opacity={0.3}
-                            depthWrite={false}
-                        >
-                            <GradientTexture
-                                attach={"alphaMap"}
-                                stops={[0, 1]}
-                                colors={["black", "white", "white"]}
+                    <group
+                        scale={[4, 1, 1]}
+                        rotation={[0, Math.PI / 4, 0]}>
+                        <mesh rotation={[0, -Math.PI / 4, 0]} >
+                            <cylinderGeometry args={[0.1, 0.1, 10, 4, 1, true]}
                             />
-                        </meshBasicMaterial>
-                    </mesh>
-                    <mesh rotation={[0, -Math.PI / 4, 0]}>
-                        <boxGeometry args={[0.5, 10, 0.1]} />
-                        <meshBasicMaterial
-                            side={DoubleSide}
-                            color={"#FF6F1E"}
-                            transparent
-                            opacity={0.3}
-                            depthWrite={false}
-                        >
-                            <GradientTexture
-                                attach={"alphaMap"}
-                                stops={[0, 1]}
-                                colors={["black", "white", "white"]}
+                            <meshBasicMaterial
+                                side={DoubleSide}
+                                color={"#FF6F1E"}
+                                transparent
+                                opacity={0.3}
+                                depthWrite={false}
+                            >
+                                <GradientTexture
+                                    attach={"alphaMap"}
+                                    stops={[0, 1]}
+                                    colors={["black", "white", "white"]}
+                                />
+                            </meshBasicMaterial>
+                        </mesh>
+                    </group>
+                    <group
+                        scale={[4, 1, 1]}
+                        rotation={[0, -Math.PI / 4, 0]}>
+                        <mesh rotation={[0, -Math.PI / 4, 0]}>
+                            <cylinderGeometry args={[0.1, 0.1, 10, 4, 1, true]}
                             />
-                        </meshBasicMaterial>
-                    </mesh>
+                            <meshBasicMaterial
+                                side={DoubleSide}
+                                color={"#FF6F1E"}
+                                transparent
+                                opacity={0.3}
+                                depthWrite={false}
+                            >
+                                <GradientTexture
+                                    attach={"alphaMap"}
+                                    stops={[0, 1]}
+                                    colors={["black", "white", "white"]}
+                                />
+                            </meshBasicMaterial>
+                        </mesh>
+                    </group>
                 </group>;
             }
         })}
